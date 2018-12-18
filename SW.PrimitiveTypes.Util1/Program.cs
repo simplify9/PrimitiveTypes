dc1014fab2a1193b72b0b73ce02c32f7f5b80a28
Samer Awajan <samer@simplify9.com>
2018-12-18 12:25:06 +0400

    Util for testing culture enums

---
+++ SW.PrimitiveTypes.Util1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace SW.PrimitiveTypes.Util1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> hashSet = new HashSet<string>();
+            foreach (CultureInfo item in CultureInfo.GetCultures(CultureTypes.AllCultures))
+                hashSet.Add(item.TwoLetterISOLanguageName);
+
+            foreach (string currency in hashSet)
+                Console.WriteLine(currency);
+
+            Console.ReadLine();
+        }
+    }
+}
