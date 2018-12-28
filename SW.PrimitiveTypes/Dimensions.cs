b32dfe47424ce6e5c28eb26c6c218357f0987c4a
Samer Awajan <samer@simplify9.com>
2018-12-29 00:14:04 +0200

    Dimensions prop public

--- SW.PrimitiveTypes/Dimensions.cs
+++ SW.PrimitiveTypes/Dimensions.cs
@@ -24,11 +24,11 @@ namespace SW.PrimitiveTypes
             Unit = unit;
         }
 
-        public decimal? Length { get; private set; }
-        public decimal? Width { get; private set; }
-        public decimal? Height { get; private set; }
+        public decimal? Length { get;  set; }
+        public decimal? Width { get;  set; }
+        public decimal? Height { get;  set; }
 
-        public DimensionUnit Unit { get; private set; }
+        public DimensionUnit Unit { get;  set; }
         
 
 
