bdabf95698c3cb74ebbcef661475e5545b2f6fe8
Samer Awajan <samer@simplify9.com>
2019-01-29 15:55:10 +0200

    Adding Audit type

---
+++ SW.PrimitiveTypes/Audit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class Audit
+    {
+
+        public Audit()
+        {
+        }
+
+        public DateTime CreatedOn { get; private set; }
+        public DateTime ModifiedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+
+
+
+    }
+}
