e503b5df33ded145e85f34eac0df724bc3fe6f51
yaser <yaser@simplify9.com>
2019-04-10 12:02:45 +0300

    ModifiedOn became a nullable datetime

--- SW.PrimitiveTypes/Audit.cs
+++ SW.PrimitiveTypes/Audit.cs
@@ -12,7 +12,7 @@ namespace SW.PrimitiveTypes
         }
 
         public DateTime CreatedOn { get; set; }
-        public DateTime ModifiedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
 
         public string CreatedBy { get; set; }
         public string ModifiedBy { get; set; }
