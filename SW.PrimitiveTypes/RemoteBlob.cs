2fcdb54f7872819091117375ebc949802b43c833
Samer Awajan <samer@simplify9.com>
2019-10-26 00:35:42 +0200

    added remote blob type to be used by DTOs or else

---
+++ SW.PrimitiveTypes/RemoteBlob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class RemoteBlob
+    {
+        public Uri Uri { get; set; }
+        public int Size { get; set; }
+        public string MimeType { get; set; }
+        public string Name { get; set; }
+
+    }
+}
