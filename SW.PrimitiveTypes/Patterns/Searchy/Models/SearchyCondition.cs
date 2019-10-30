a645545897db835e44503ff931d9ac2e2de39b34
Samer Awajan <samer@simplify9.com>
2019-10-30 22:53:31 +0200

    condition collection contructors fixes

--- SW.PrimitiveTypes/Patterns/Searchy/Models/SearchyCondition.cs
+++ SW.PrimitiveTypes/Patterns/Searchy/Models/SearchyCondition.cs
@@ -15,19 +15,29 @@ namespace SW.PrimitiveTypes
             Filters.Add(new SearchyFilter(field, rule, value)); 
         }
 
-        public SearchyCondition(ISearchyFilter filter)
+        //public SearchyCondition(ISearchyFilter filter)
+        //{
+        //    Filters = new List<SearchyFilter> { new SearchyFilter(filter) };
+        //}
+
+        public SearchyCondition(ISearchyFilterTyped filter)
         {
             Filters = new List<SearchyFilter> { new SearchyFilter(filter) };
         }
 
-        public SearchyCondition(ICollection<ISearchyFilterTyped> filters)
+        public SearchyCondition(IEnumerable<SearchyFilter> filters)
+        {
+            Filters = filters.ToList();
+        }
+
+        public SearchyCondition(IEnumerable<ISearchyFilterTyped> filters)
         {
             Filters = filters.Select(f => new SearchyFilter(f)).ToList();
         }
 
-        public SearchyCondition(ICollection<ISearchyFilter> filters)
-        {
-            Filters = filters.Select(f => new SearchyFilter(f)).ToList();
-        }
+        //public SearchyCondition(IEnumerable<ISearchyFilter> filters)
+        //{
+        //    Filters = filters.Select(f => new SearchyFilter(f)).ToList();
+        //}
     }
 }
