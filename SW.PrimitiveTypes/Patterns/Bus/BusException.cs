89ea6d58b0e1bcd7c805ba6d2ca74e9ee165aa41
Samer Awajan <samer@simplify9.com>
2019-10-31 15:23:26 +0200

    busexception

---
+++ SW.PrimitiveTypes/Patterns/Bus/BusException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class BusException : Exception
+    {
+        public BusException(string message) : base(message)
+        {
+        }
+
+        public BusException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
