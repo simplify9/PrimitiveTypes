0a5af3a43b4dac1ebf9896b6043ba16d2b244be7
Samer Awajan <samer@simplify9.com>
2019-12-07 00:48:04 +0100

    RequestContext

---
+++ SW.PrimitiveTypes/Patterns/RequestContext/IRequestContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public interface IRequestContext
+    {
+        ClaimsPrincipal User { get; }
+        IReadOnlyCollection<RequestValue> Values { get; }
+        bool IsValid { get; }
+    }
+}
---
+++ SW.PrimitiveTypes/Patterns/RequestContext/RequestValue.cs
@@ -0,0 +1,27 @@
+﻿using SW.PrimitiveTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class RequestValue : ValueObject
+    {
+        public RequestValue(string name, string value, RequestValueType type)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+            Type = type;
+        }
+
+        public string Name { get;  }
+        public string Value { get;  }
+        public RequestValueType Type { get;  }
+    }
+
+    public enum RequestValueType
+    {
+        QueryParameter,
+        HttpHeader,
+    }
+}
