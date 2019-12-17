0bf05e743c92124556629b1edad991a8065606a9
Muhannad Khatib <muhannad@simplify9.com>
2019-12-17 09:17:07 +0200

    Volumetric weight and dimensions conversions

---
+++ SW.PrimitiveTypes.UnitTests/DimensionConversionTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SW.PrimitiveTypes.UnitTests
+{
+    [TestClass]
+    public class DimensionConversionTests
+    {
+        [TestMethod]
+        public void FromCmToM()
+        {
+            var dim = new Dimensions(300, 300, 300, DimensionUnit.cm);
+            var dimInM = dim.Convert(DimensionUnit.M);
+
+            Assert.AreEqual(dimInM.Height, 3m);
+        }
+
+        [TestMethod]
+        public void FromMToCm()
+        {
+            var dim = new Dimensions(2, 2, 2, DimensionUnit.M);
+            var dimInCm = dim.Convert(DimensionUnit.cm);
+
+            Assert.AreEqual(dimInCm.Height, 200);
+        }
+
+        [TestMethod]
+        public void FromInToCm()
+        {
+            var dim = new Dimensions(2, 2, 2, DimensionUnit.@in);
+            var dimInCm = dim.Convert(DimensionUnit.cm);
+
+            Assert.AreEqual(dimInCm.Height,5.08m);
+        }
+
+        [TestMethod]
+        public void FromCmToIn()
+        {
+            var dim = new Dimensions(10, 10, 10, DimensionUnit.cm);
+            var dimInIn = dim.Convert(DimensionUnit.@in);
+
+            Assert.AreEqual(dimInIn.Height, 3.937008m);
+        }
+        
+        [TestMethod]
+        public void FromInToM()
+        {
+            var dim = new Dimensions(200, 200, 200, DimensionUnit.@in);
+            var dimInM = dim.Convert(DimensionUnit.M);
+
+            Assert.AreEqual(dimInM.Height, 5.08001m);
+        }
+
+        [TestMethod]
+        public void FromMToIn()
+        {
+            var dim = new Dimensions(5, 5, 5, DimensionUnit.M);
+            var dimInIn = dim.Convert(DimensionUnit.@in);
+
+            Assert.AreEqual(dimInIn.Height, 196.85m);
+        }
+
+        [TestMethod]
+        public void FromToSame()
+        {
+            var dim = new Dimensions(5, 5, 5, DimensionUnit.M);
+            var dimInM = dim.Convert(DimensionUnit.M);
+
+            Assert.AreEqual(dimInM.Height, 5m);
+        }
+
+        [TestMethod]
+        public void VolumetricWeightCalculation()
+        {
+            var dimInM = new Dimensions(5, 5, 5, DimensionUnit.M);
+            var volumetricWeight = dimInM.GetVolumetricWeight();
+
+            Assert.AreEqual(volumetricWeight.Unit, WeightUnit.kg);
+            Assert.AreEqual(volumetricWeight.Value, 250m);
+
+
+            var dimInCm = new Dimensions(10, 10, 5, DimensionUnit.cm);
+            volumetricWeight = dimInCm.GetVolumetricWeight();
+
+            Assert.AreEqual(volumetricWeight.Unit, WeightUnit.kg);
+            Assert.AreEqual(volumetricWeight.Value, 0.1m);
+
+            var dimInIn = new Dimensions(10, 10, 5, DimensionUnit.@in);
+            volumetricWeight = dimInIn.GetVolumetricWeight();
+
+            Assert.AreEqual(volumetricWeight.Unit, WeightUnit.lb);
+            Assert.AreEqual(volumetricWeight.Value, 3.613356m);
+        }
+    }
+}
--- SW.PrimitiveTypes/Dimensions.cs
+++ SW.PrimitiveTypes/Dimensions.cs
@@ -24,39 +24,83 @@ namespace SW.PrimitiveTypes
             Unit = unit;
         }
 
-        public decimal? Length { get;  set; }
-        public decimal? Width { get;  set; }
-        public decimal? Height { get;  set; }
+        public decimal? Length { get; set; }
+        public decimal? Width { get; set; }
+        public decimal? Height { get; set; }
 
-        public DimensionUnit? Unit { get;  set; }
-        
+        public DimensionUnit? Unit { get; set; }
+
+        public decimal? Volume => Length == null ? (decimal?)null : Length.Value * Width.Value * Height.Value;
 
 
-        public Weight GetVolumetricWeight()
+        public Weight GetVolumetricWeight(decimal? calculationFactor = null)
         {
-            if (Length == null)
-            {
-                throw new InvalidOperationException("Cannot calculate the volumetric weight from empty dimensions");
-            }
 
-            var volume = Length.Value * Width.Value * Height.Value;
-
+            decimal factor;
+            
+            WeightUnit weightUnit; 
             //6000 ccm / kg, 166 cu in/ lb, 366 cu in/ kg
             switch (Unit)
             {
                 case DimensionUnit.cm:
-                    return new Weight(volume / 6000, WeightUnit.kg);
-
+                    factor = 5000m;
+                    weightUnit = WeightUnit.kg;
+                    break;
                 case DimensionUnit.M:
-                    return new Weight(1000 * volume / 6000, WeightUnit.kg);
-
+                    weightUnit = WeightUnit.kg;
+                    factor = 0.5m;
+                    break;
                 case DimensionUnit.@in:
-                    return new Weight(volume / 166, WeightUnit.lb);
-
+                    weightUnit = WeightUnit.lb;                    
+                    factor = 138.3755m;
+                    break;
                 default:
                     throw new NotSupportedException();
             }
+
+            var volumetricWeight = Math.Round(Volume.Value / (calculationFactor ?? factor), 6);
+            return new Weight(volumetricWeight, weightUnit);
         }
+
+        public Dimensions Convert(DimensionUnit toUnit)
+        {
+            if (Length == null)
+                return Empty();
+            return new Dimensions(Convert(toUnit, Length.Value),
+                Convert(toUnit, Width.Value),
+                Convert(toUnit, Height.Value),
+                toUnit);
+        }
+
+        private decimal Convert(DimensionUnit toUnit, decimal val)
+        {
+            if (toUnit == Unit)
+                return val;
+
+            decimal result;
+            switch (Unit)
+            {
+                case DimensionUnit.cm:
+                    result = toUnit == DimensionUnit.M ? FromCmToM(val) : FromCmToIn(val);
+                    break;
+                case DimensionUnit.M:
+                    result = toUnit == DimensionUnit.cm ? FromMToCm(val) : FromMToIn(val);
+                    break;
+                case DimensionUnit.@in:
+                    result = toUnit == DimensionUnit.M ? FromInToM(val) : FromInToCm(val);
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+
+            return Math.Round(result, 6);
+        }
+        decimal FromCmToM(decimal val) => val / 100m;
+        decimal FromMToCm(decimal val) => val * 100m;
+        decimal FromCmToIn(decimal val) => val / 2.54m;
+        decimal FromInToCm(decimal val) => val * 2.54m;
+        decimal FromMToIn(decimal val) => val * 39.37m;
+        decimal FromInToM(decimal val) => val / 39.37m;
 
         public override string ToString()
         {
