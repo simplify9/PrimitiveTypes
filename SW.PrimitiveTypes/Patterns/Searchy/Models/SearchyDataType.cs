1b633b5c4d0b22a3ca62dfb46becd995ad54af43
Samer Awajan <samer@simplify9.com>
2020-01-14 01:11:24 +0200

    getting filter type from dotnet type

--- SW.PrimitiveTypes/Patterns/Searchy/Models/SearchyDataType.cs
+++ SW.PrimitiveTypes/Patterns/Searchy/Models/SearchyDataType.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System;
 using System.Collections.Generic;
 
 namespace SW.PrimitiveTypes
@@ -11,25 +12,24 @@ namespace SW.PrimitiveTypes
         public const string Number = "numeric";
         public const string Boolean = "boolean";
 
-
         public static ICollection<SearchyRule> RulesFor(string dataType)
         {
             switch (dataType)
             {
                 case Text:
 
-                        return new SearchyRule[]
-                        {
+                    return new SearchyRule[]
+                    {
                             SearchyRule.EqualsTo,
                             SearchyRule.NotEqualsTo,
                             SearchyRule.Contains,
                             SearchyRule.StartsWith,
-                        };
+                    };
 
                 case Number:
 
-                        return new SearchyRule[]
-                        {
+                    return new SearchyRule[]
+                    {
                             SearchyRule.EqualsTo,
                             SearchyRule.NotEqualsTo,
                             SearchyRule.LessThan,
@@ -37,30 +37,126 @@ namespace SW.PrimitiveTypes
                             SearchyRule.GreaterThan,
                             SearchyRule.GreaterThanOrEquals,
 
-                        };
+                    };
 
                 case Date:
 
-                        return new SearchyRule[]
-                        {
+                    return new SearchyRule[]
+                    {
                             SearchyRule.LessThan,
                             SearchyRule.LessThanOrEquals,
                             SearchyRule.GreaterThan,
                             SearchyRule.GreaterThanOrEquals,
-                        };
+                    };
 
                 case Boolean:
 
-                        return new SearchyRule[]
-                        {
+                    return new SearchyRule[]
+                    {
                             SearchyRule.EqualsTo
-                        };
+                    };
 
             }
 
             return null;
 
         }
+
+        public static string TypeStringFor(Type type)
+        {
+            var nakedType = Nullable.GetUnderlyingType(type) ?? type;
+
+            switch (Type.GetTypeCode(nakedType.GetType()))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+
+                    return Number;
+
+                case TypeCode.DateTime:
+
+                    return Date;
+
+                case TypeCode.Boolean:
+
+                    return Boolean;
+
+                default:
+
+                    return Text;
+            }
+        }
+
+
+        //public static ICollection<SearchyRule> RulesFor(Type type)
+        //{
+
+        //    var nakedType = Nullable.GetUnderlyingType(type) ?? type;
+
+        //    switch (Type.GetTypeCode(nakedType.GetType()))
+        //    {
+        //        case TypeCode.Byte:
+        //        case TypeCode.SByte:
+        //        case TypeCode.UInt16:
+        //        case TypeCode.UInt32:
+        //        case TypeCode.UInt64:
+        //        case TypeCode.Int16:
+        //        case TypeCode.Int32:
+        //        case TypeCode.Int64:
+        //        case TypeCode.Decimal:
+        //        case TypeCode.Double:
+        //        case TypeCode.Single:
+
+        //            return new SearchyRule[]
+        //                {
+        //                    SearchyRule.EqualsTo,
+        //                    SearchyRule.NotEqualsTo,
+        //                    SearchyRule.LessThan,
+        //                    SearchyRule.LessThanOrEquals,
+        //                    SearchyRule.GreaterThan,
+        //                    SearchyRule.GreaterThanOrEquals,
+
+        //                };
+
+        //        case TypeCode.DateTime:
+
+        //            return new SearchyRule[]
+        //            {
+        //                    SearchyRule.LessThan,
+        //                    SearchyRule.LessThanOrEquals,
+        //                    SearchyRule.GreaterThan,
+        //                    SearchyRule.GreaterThanOrEquals,
+        //            };
+
+        //        case TypeCode.Boolean:
+
+        //            return new SearchyRule[]
+        //            {
+        //                    SearchyRule.EqualsTo
+        //            };
+
+        //        default:
+
+        //            return new SearchyRule[]
+        //            {
+        //                    SearchyRule.EqualsTo,
+        //                    SearchyRule.NotEqualsTo,
+        //                    SearchyRule.Contains,
+        //                    SearchyRule.StartsWith,
+        //            };
+        //    }
+
+        //}
+
     };
 
 
