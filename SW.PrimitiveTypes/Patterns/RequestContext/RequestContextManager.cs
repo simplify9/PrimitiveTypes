bff5957a1fc005b84e4c4497edcc857b5e6ce4de
Samer Awajan <samer@simplify9.com>
2020-01-30 18:35:48 +0200

    requestcontextmanager

---
+++ SW.PrimitiveTypes/Patterns/RequestContext/RequestContextManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class RequestContextManager
+    {
+        private readonly IEnumerable<IRequestContext> requestContexts;
+
+        public RequestContextManager(IEnumerable<IRequestContext> requestContexts)
+        {
+            this.requestContexts = requestContexts;
+        }
+
+        public IRequestContext Current => requestContexts.Where(e => e.IsValid).SingleOrDefault();
+
+    }
+}
