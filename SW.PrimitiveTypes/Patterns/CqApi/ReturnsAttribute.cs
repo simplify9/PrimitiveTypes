458322befcc122d87a490435821bfe1e5299cdef
Shaheen Sarafa <shaheen@simplify9.com>
2020-02-04 10:13:05 +0200

    moved protect and added returns attribute

---
+++ SW.PrimitiveTypes/Patterns/CqApi/ReturnsAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SW.PrimitiveTypes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public sealed class ReturnsAttribute : Attribute
+    {
+        public Type Type;
+        public int StatusCode;
+        public string Conditions;
+    }
+}
