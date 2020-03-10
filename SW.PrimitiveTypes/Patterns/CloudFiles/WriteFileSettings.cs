7d20d8d798e6097047b4d88f6859ef509045424d
Samer Zughul <samerz@simplify9.com>
2020-03-10 16:23:24 +0200

    cloudfiles models

---
+++ SW.PrimitiveTypes/Patterns/CloudFiles/WriteFileSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes.Patterns.CloudFiles
+{
+    public class WriteFileSettings
+    {
+        public string Key { get; set; }
+        public string ContentType { get; set; }
+        public bool Public { get; set; }
+    }
+}
