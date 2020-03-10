4a73141b3adb8277ceda6ef5fbce4b1be3a6b400
Samer Zughul <samerz@simplify9.com>
2020-03-10 16:25:26 +0200

    cloudfiles settings model namespace update

--- SW.PrimitiveTypes/Patterns/CloudFiles/WriteFileSettings.cs
+++ SW.PrimitiveTypes/Patterns/CloudFiles/WriteFileSettings.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SW.PrimitiveTypes.Patterns.CloudFiles
-{
+namespace SW.PrimitiveTypes
+{ 
     public class WriteFileSettings
     {
         public string Key { get; set; }
