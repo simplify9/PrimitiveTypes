d10770e223631e84eadf649a7d320b93d83a2272
Samer Zughul <samerz@simplify9.com>
2020-03-11 11:09:03 +0200

    icloudservicefiles pattern

---
+++ SW.PrimitiveTypes/Patterns/CloudFiles/ICloudFilesService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.PrimitiveTypes
+{ 
+    public interface ICloudFilesService
+    {
+        /// <summary>
+        ///  Method to write a file asynchronously (privately or publicly)
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        Task WriteAcync(Stream inputStream, WriteFileSettings settings);
+       
+
+    }
+}
