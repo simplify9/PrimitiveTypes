79cc5a805518268982b034e0899d7c42c694e5b4
Samer Awajan <samer@simplify9.com>
2020-04-05 20:57:58 +0300

    new query handler

--- SW.PrimitiveTypes/Patterns/CqApi/IQueryHandler.cs
+++ SW.PrimitiveTypes/Patterns/CqApi/IQueryHandler.cs
@@ -2,6 +2,11 @@
 
 namespace SW.PrimitiveTypes
 {
+    public interface IQueryHandler<in TKey, in TRequest> : IHandler where TRequest : class
+    {
+        Task<object> Handle(TKey key, TRequest request);
+    }
+
     public interface IQueryHandler<in TRequest> : IHandler where TRequest : class
     {
         Task<object> Handle(TRequest request);
