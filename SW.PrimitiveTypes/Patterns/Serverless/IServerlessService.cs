e7a38b0b4931b086f1a0b89d7bebf1590601c891
Samer Awajan <samer@simplify9.com>
2020-04-15 20:53:12 +0300

    serveless overloads

--- SW.PrimitiveTypes/Patterns/Serverless/IServerlessService.cs
+++ SW.PrimitiveTypes/Patterns/Serverless/IServerlessService.cs
@@ -8,7 +8,12 @@ namespace SW.PrimitiveTypes
     public interface IServerlessService
     {
         Task StartAsync(string adapterId, string[] arguments = null);
+        Task StartAsync(string adapterId, string adapterPath, string[] arguments = null);
+
         Task InvokeVoidAsync(string command, string input = null);
+        Task InvokeVoidAsync(string command, int commandTimeout, string input = null);
+
         Task<string> InvokeAsync(string command, string input = null);
+        Task<string> InvokeAsync(string command, int commandTimeout, string input = null);
     }
 }
