700b0d1ad4e5cbba75da2e52e2db853a27d45e63
Samer Awajan <samer@simplify9.com>
2020-04-24 18:23:17 +0300

    ..

---
+++ SW.PrimitiveTypes/Patterns/Serverless/ServerlessOptions.cs
@@ -0,0 +1,18 @@
+﻿using SW.PrimitiveTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class ServerlessOptions
+    {
+
+        public CloudFilesOptions CloudFilesOptions { get; set; }
+        public string AdapterLocalPath { get; set; } = "./adapters";
+        public int AdapterMetadataCacheDuration { get; set; } = 5;
+        public string AdapterRemotePath { get; set; } = "adapters";
+        public int CommandTimeout { get; set; } = 30;
+        public int IdleTimeout { get; set; } = 300;
+    }
+}
