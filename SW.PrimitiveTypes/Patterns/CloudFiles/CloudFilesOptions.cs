e69b99d0376f1179644ba58c15a67a9f7b35b75c
Samer Awajan <samer@simplify9.com>
2020-04-24 16:31:16 +0300

    CloudFilesOptions

---
+++ SW.PrimitiveTypes/Patterns/CloudFiles/CloudFilesOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SW.PrimitiveTypes
+{
+    public class CloudFilesOptions
+    {
+
+        public const string ConfigurationSection = "CloudFiles";
+
+        public string AccessKeyId { get; set; }
+        public string SecretAccessKey { get; set; }
+        public string BucketName { get; set; }
+        public string ServiceUrl { get; set; }
+    }
+}
