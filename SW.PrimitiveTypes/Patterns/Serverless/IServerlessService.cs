95336791c6b590e071a816c13ab566fa93e596e7
Samer Awajan <samer@simplify9.com>
2020-04-24 23:37:44 +0300

    ..

--- SW.PrimitiveTypes/Patterns/Serverless/IServerlessService.cs
+++ SW.PrimitiveTypes/Patterns/Serverless/IServerlessService.cs
@@ -7,8 +7,8 @@ namespace SW.PrimitiveTypes
 {
     public interface IServerlessService
     {
-        Task StartAsync(string adapterId, string[] arguments = null);
-        Task StartAsync(string adapterId, string adapterPath, string[] arguments = null);
+        Task StartAsync(string adapterId, IDictionary<string, string> startupValues = null);
+        Task StartAsync(string adapterId, string adapterPath, IDictionary<string, string> startupValues = null);
 
         Task InvokeVoidAsync(string command, string input = null);
         Task InvokeVoidAsync(string command, int commandTimeout, string input = null);
