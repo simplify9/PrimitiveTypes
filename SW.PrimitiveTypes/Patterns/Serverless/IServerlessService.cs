358f76645e571e3a940a5b317afada566785de67
Samer Awajan <samer@simplify9.com>
2020-04-30 10:47:50 +0300

    ..

--- SW.PrimitiveTypes/Patterns/Serverless/IServerlessService.cs
+++ SW.PrimitiveTypes/Patterns/Serverless/IServerlessService.cs
@@ -13,7 +13,7 @@ namespace SW.PrimitiveTypes
         Task InvokeVoidAsync(string command, string input = null);
         Task InvokeVoidAsync(string command, int commandTimeout, string input = null);
 
-        Task<string> InvokeAsync(string command, string input = null);
-        Task<string> InvokeAsync(string command, int commandTimeout, string input = null);
+        Task<TResult> InvokeAsync<TResult>(string command, object input = null);
+        Task<TResult> InvokeAsync<TResult>(string command, int commandTimeout, object input = null);
     }
 }
