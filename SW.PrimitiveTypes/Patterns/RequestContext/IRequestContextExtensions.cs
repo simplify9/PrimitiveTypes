aad8f288fd6c50b73a97c07c634a68536867aac9
Samer Awajan <samer@simplify9.com>
2020-06-07 15:27:29 +0300

    request context extensions

---
+++ SW.PrimitiveTypes/Patterns/RequestContext/IRequestContextExtensions.cs
@@ -0,0 +1,33 @@
+﻿using SW.PrimitiveTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    internal static class IRequestContextExtensions
+    {
+
+        public static string GetNameIdentifier(this IRequestContext requestContext)
+        {
+            return requestContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        public static string GetEntity(this IRequestContext requestContext)
+        {
+            return requestContext.User.FindFirst("Entity").Value;
+        }
+
+        public static bool HasGlobalAccess(this IRequestContext requestContext)
+        {
+            return bool.Parse(requestContext.User.FindFirst("GlobalAccess").Value);
+        }
+
+        public static IEnumerable<string> GetAllowedEntities(this IRequestContext requestContext)
+        {
+            return requestContext.User.FindAll("AllowedEntity").Select(c => c.Value).Union(new[] { requestContext.User.FindFirst("Entity").Value });
+        }
+    }
+}
