203887627e1ce081b52d749875565d515fb90740
Samer Awajan <samer@simplify9.com>
2020-06-14 11:13:50 +0300

    adding infolink pattern

---
+++ SW.PrimitiveTypes/Patterns/Infolink/IInfolinkHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.PrimitiveTypes
+{
+    public interface IInfolinkHandler
+    {
+        Task<object> Handle(XchangeFile xchangeFile);
+    }
+}
