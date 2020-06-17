eb932691f5e6380f3f17a97d1230767cb41c2a58
Samer Awajan <samer@simplify9.com>
2020-06-17 12:05:56 +0300

    adding reporty model

---
+++ SW.PrimitiveTypes/Patterns/Reporty/DataSourceType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public enum DataSourceType
+    {
+        MsSql = 1,
+        MySql = 2,
+        SQLite = 3
+    }
+}
---
+++ SW.PrimitiveTypes/Patterns/Reporty/ExportRequest.cs
@@ -0,0 +1,15 @@
+﻿using SW.PrimitiveTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class ExportRequest
+    {
+        public CloudFilesOptions CloudFilesOptions { get; set; }
+        public string ReportLocation { get; set; }
+        public ReportDataSource DataSource { get; set; }
+        public IDictionary<string, string> Parameters { get; set; }
+    }
+}
