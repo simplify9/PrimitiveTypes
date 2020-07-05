05198ff8db34aaa2c5715435ccd314b63bd906e4
Samer Awajan <samer@simplify9.com>
2020-07-05 22:17:27 +0300

    refactoring and tenancy interfaces

---
+++ SW.PrimitiveTypes/Patterns/Domain/CommonProperties/IHasOptionalTenant.cs
@@ -0,0 +1,13 @@
+﻿namespace SW.PrimitiveTypes
+{
+    /// <summary>
+    /// Implement this interface for an entity which may optionally have TenantId.
+    /// </summary>
+    public interface IHasOptionalTenant
+    {
+        /// <summary>
+        /// TenantId of this entity.
+        /// </summary>
+        int? TenantId { get; set; }
+    }
+}
