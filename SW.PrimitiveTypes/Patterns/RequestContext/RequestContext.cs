8ef2e29fc605b5b70922e4d5a612bad0bcd14bce
Samer Awajan <samer@simplify9.com>
2020-07-08 15:39:55 +0300

    ..

--- SW.PrimitiveTypes/Patterns/RequestContext/RequestContext.cs
+++ SW.PrimitiveTypes/Patterns/RequestContext/RequestContext.cs
@@ -7,6 +7,13 @@ namespace SW.PrimitiveTypes
 {
     public class RequestContext
     {
+        public RequestContext(ClaimsPrincipal user, IReadOnlyCollection<RequestValue> values, string correlationId)
+        {
+            User = user;
+            Values = values;
+            CorrelationId = correlationId;
+        }
+
         public ClaimsPrincipal User { get; }
         public IReadOnlyCollection<RequestValue> Values { get; }
         public string CorrelationId { get; }
