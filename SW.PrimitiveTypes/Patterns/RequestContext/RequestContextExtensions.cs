9a94afb3614a68dc3a78298530d537b84f3e5490
Samer Awajan <samer@simplify9.com>
2020-07-08 15:17:48 +0300

    refactoring request context

--- SW.PrimitiveTypes/Patterns/RequestContext/RequestContextExtensions.cs
+++ SW.PrimitiveTypes/Patterns/RequestContext/RequestContextExtensions.cs
@@ -7,31 +7,31 @@ using System.Text;
 
 namespace SW.PrimitiveTypes
 {
-    public static class IRequestContextExtensions
+    public static class RequestContextExtensions
     {
 
-        public static string GetNameIdentifier(this IRequestContext requestContext)
+        public static string GetNameIdentifier(this RequestContext requestContext)
         {
             return requestContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
 
-        public static string GetEntity(this IRequestContext requestContext)
+        public static string GetEntity(this RequestContext requestContext)
         {
             return requestContext.User.FindFirst("Entity")?.Value;
         }
 
-        public static int GetTenant(this IRequestContext requestContext)
+        public static int GetTenant(this RequestContext requestContext)
         {
             int.TryParse(requestContext.User.FindFirst("Tenant")?.Value, out var tenant);
             return tenant;
         }
 
-        public static bool HasGlobalAccess(this IRequestContext requestContext)
+        public static bool HasGlobalAccess(this RequestContext requestContext)
         {
             return bool.Parse(requestContext.User.FindFirst("GlobalAccess").Value);
         }
 
-        public static IEnumerable<string> GetAllowedEntities(this IRequestContext requestContext)
+        public static IEnumerable<string> GetAllowedEntities(this RequestContext requestContext)
         {
             return requestContext.User.FindAll("AllowedEntity").Select(c => c.Value).Union(new[] { requestContext.User.FindFirst("Entity").Value });
         }
