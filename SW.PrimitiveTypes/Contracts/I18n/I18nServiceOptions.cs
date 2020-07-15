9462e66e94644d1b97920543f2cb177d3afee9e7
Samer Awajan <samer@simplify9.com>
2020-07-15 12:02:57 +0300

    ..

---
+++ SW.PrimitiveTypes/Contracts/I18n/I18nServiceOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class I18nServiceOptions
+    {
+        public const string ConfigurationSection = "I18n";
+        public string CurrencyRatesUrl { get; set; }
+        public int CurrencyRatesCacheDuration { get; set; } = 60;
+        public string BaseCurrency { get; set; } = "usd";
+    }
+}
