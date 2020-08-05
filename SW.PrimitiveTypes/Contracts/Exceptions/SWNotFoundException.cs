484028a3326cf0bb088b029fca1e549ea8ff1690
Samer Awajan <samer@simplify9.com>
2020-08-05 15:07:25 +0300

    not found exception

---
+++ SW.PrimitiveTypes/Contracts/Exceptions/SWNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class SWNotFoundException : SWException
+    {
+        public SWNotFoundException()
+        {
+        }
+
+        public SWNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
