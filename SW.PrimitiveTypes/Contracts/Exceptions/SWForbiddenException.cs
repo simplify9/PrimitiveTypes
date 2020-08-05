9c67e7703cb998c847bab4bb1705ac57ba3b4c5f
Samer Awajan <samer@simplify9.com>
2020-08-05 14:48:19 +0300

    adding exception types and request context minor refactor

---
+++ SW.PrimitiveTypes/Contracts/Exceptions/SWForbiddenException.cs
@@ -0,0 +1,16 @@
+﻿namespace SW.PrimitiveTypes
+{
+    public class SWForbiddenException : SWException
+    {
+        public SWForbiddenException()
+        {
+        }
+
+        public SWForbiddenException(string message) : base(message)
+        {
+        }
+    }
+
+
+
+}
---
+++ SW.PrimitiveTypes/Contracts/Exceptions/SWUnauthorizedException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class SWUnauthorizedException : SWException
+    {
+        public SWUnauthorizedException()
+        {
+        }
+
+        public SWUnauthorizedException(string message) : base(message)
+        {
+        }
+    }
+
+
+
+}
