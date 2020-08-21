8ec20c45ff9bb73e799bae8f7c3ca769f3e7ce41
Samer Awajan <samer@simplify9.com>
2020-08-21 12:22:03 +0300

    moving apiresult from httpextensions

---
+++ SW.PrimitiveTypes/Contracts/Http/ApiResult.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SW.PrimitiveTypes
+{
+    public class ApiResult
+    {
+        public bool Success { get; set; }
+        public int StatusCode { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class ApiResult<TResponse> : ApiResult
+    {
+        public TResponse Response { get; set; }
+    }
+}
