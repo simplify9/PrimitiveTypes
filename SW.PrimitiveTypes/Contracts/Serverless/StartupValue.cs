3f964e848893ad8b0fcad34c1557bf5e8f54f49e
Samer Awajan <samer@simplify9.com>
2020-09-04 19:51:13 +0300

    serverless features

---
+++ SW.PrimitiveTypes/Contracts/Serverless/StartupValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.PrimitiveTypes
+{
+    public class StartupValue
+    {
+        public bool Optional { get; set; }
+        public string Default { get; set; }
+        public string Type { get; set; }
+    }
+}
