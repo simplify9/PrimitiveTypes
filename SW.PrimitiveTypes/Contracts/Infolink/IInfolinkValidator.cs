0ab2bcc219049fac0e3a4a67918ccbd2ad859bd6
Samer Awajan <samer@simplify9.com>
2020-09-08 13:46:43 +0300

    infolink validator

---
+++ SW.PrimitiveTypes/Contracts/Infolink/IInfolinkValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.PrimitiveTypes
+{
+    public interface IInfolinkValidator
+    {
+        Task<InfolinkValidatorResult> Validate(XchangeFile xchangeFile);
+    }
+}
