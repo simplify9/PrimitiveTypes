edcb04b5dddb378b5bbfc8789dbd96feaa9d862a
Samer Awajan <samer@simplify9.com>
2020-10-28 16:57:50 +0300

    atomic counter and string extensions

---
+++ SW.PrimitiveTypes/Contracts/AtomicCounter/IAtomicCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.PrimitiveTypes
+{
+    public interface IAtomicCounter
+    {
+        Task<long> IncrementAsync(string counterName);
+
+        Task ResetAsync(string counterName);
+
+    }
+}
---
+++ SW.PrimitiveTypes/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SW.PrimitiveTypes
+{
+    static class StringExtensions
+    {
+
+        public static string NullIfEmpty(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return null;
+            return input;
+        }
+
+        public static string EmptyIfNull(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+            return input;
+        }
+
+    }
+}
