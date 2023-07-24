d5cb7ad36a4ba4ec013fd09bc4094e8e899065cd
SamerZee <samer.zughul@hotmail.com>
2023-07-24 22:04:11 +0300

    handlers result generic

--- SW.PrimitiveTypes/Contracts/CqApi/IDeleteHandler.cs
+++ SW.PrimitiveTypes/Contracts/CqApi/IDeleteHandler.cs
@@ -2,11 +2,7 @@
 
 namespace SW.PrimitiveTypes
 {
-    public interface IDeleteHandler<in TKey> : IHandler
-    {
-        Task<object> Handle(TKey key);
-    }
-    
+
     public interface IDeleteHandler<in TKey, TResult> : IHandler
     {
         Task<TResult> Handle(TKey key);
--- SW.PrimitiveTypes/Contracts/CqApi/IGetHandler.cs
+++ SW.PrimitiveTypes/Contracts/CqApi/IGetHandler.cs
@@ -2,11 +2,6 @@
 
 namespace SW.PrimitiveTypes
 {
-    public interface IGetHandler<in TKey> : IHandler
-    {
-        Task<object> Handle(TKey key, bool lookup = false);
-    }
-    
     public interface IGetHandler<in TKey, TResult> : IHandler
     {
         Task<TResult> Handle(TKey key);
